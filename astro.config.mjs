cbc1a0d11d016c315ac4d1a25b77b4bec2eaba2a
Jose Negrin <soyjosenegrin@gmail.com>
2024-05-21 19:25:23 +0200

    init deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 
 export default defineConfig({
+  build: {
+		inlineStylesheets: "always",
+	},
+	compressHTML: true,
+	prefetch: true,
   devToolbar: {
     enabled: false
   },
   integrations: [tailwind({
     applyBaseStyles: false
-  })]
+  })],
+  output: "static",
+  adapter: vercelStatic()
 });
